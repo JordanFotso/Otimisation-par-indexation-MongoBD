6d9dc0da97c0c09d56dcd248d1b777d3e0cc12e0
JordanFotso <achillefotso43@gmail.com>
2026-05-12 19:23:15 +0100

    feat:initialisation de la connection a la bd

--- serveur/src/index.ts
+++ serveur/src/index.ts
@@ -1,6 +1,10 @@
 import app from "./app";
+import { connectDB } from "./config/database";
 
 const PORT = process.env.PORT || 3001;
+
+// Connexion à la base de données
+connectDB();
 
 app.listen(PORT, () => {
   console.log(`[server]: Server is running at http://localhost:${PORT}`);
