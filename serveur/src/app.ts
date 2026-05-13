31b36660914ada8d9533fea655c6485cf538fe1c
JordanFotso <achillefotso43@gmail.com>
2026-05-13 04:35:05 +0100

    fix:erreur cors

--- serveur/src/app.ts
+++ serveur/src/app.ts
@@ -11,6 +11,7 @@ app.use(helmet());
 app.use(cors({
   origin: "http://localhost:5173",
   methods: ["GET", "POST", "PATCH", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true
 }));
 app.use(express.json());
